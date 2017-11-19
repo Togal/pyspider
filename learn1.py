5708ab44f45efe8d42c01196c0a97f4466a029ae
Togal <764824948@qq.com>
2017-11-19 15:33:48 -0500

    modified:   learn1.py

--- learn1.py
+++ learn1.py
@@ -1,5 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(),"html.parser")
-print(bsObj.h1)
+from urllib.error import HTTPError
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(),"html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+url = "http://www.pythonscraping.com/pages/page1.html"
+title = getTitle(url)
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
