150eac73d4700cc64f555e225c5a3eeb2983d5a0
Togal <764824948@qq.com>
2017-11-19 17:08:19 -0500

    modified:   learn2.py

--- learn2.py
+++ learn2.py
@@ -5,9 +5,16 @@ from urllib.error import HTTPError
 url = "http://www.pythonscraping.com/pages/warandpeace.html"
 html = urlopen(url)
 bsObj = BeautifulSoup(html.read(),"html.parser")
+
+#查找符合标签的内容
 nameList = bsObj.findAll("span",{"class":"green"})
+#bsObj.findAll(id="text")
+#bsObj.findAll("", {"id":"text"})
+#bsObj.findAll(class_="green")
 
 for name in nameList:
     print(name.get_text())
 
-
+#查找网页文本内容
+textList = bsObj.findAll(text="the prince")
+print(len(textList))
