855c13da84024fe3ba729ccad1c275d6c6204949
Togal <764824948@qq.com>
2017-11-19 16:53:07 -0500

    new file:   learn4.py 	modified:   learn2.py

---
+++ learn4.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+import re
+
+#导航树,父子标签
+#bsObj.body.h1  选择body标签后代第一个h1标签
+#bsObj.div.findAll("img") 找出文档第一个div，然后获取div后代里所有img标签列表
+
+url = "http://www.pythonscraping.com/pages/page3.html"
+html = urlopen(url)
+bsObj = BeautifulSoup(html.read(),"html.parser")
+
+images = bsObj.findAll("img",{"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+for image in images:
+    print(image["src"])
